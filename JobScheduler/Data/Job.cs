048c1335a4825d975a644bbcb751e524735f84e3
Alessandro Spisso <5341363+ilGianfri@users.noreply.github.com>
2020-05-26 14:28:15 +0200

    Created Job and Note objects

---
+++ JobScheduler/Data/Job.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobScheduler.Data
+{
+    public class Job
+    {
+        public string Path { get; set; }
+        public string Parameters { get; set; }
+        public Node[] Nodes { get; set; }
+    }
+}
---
+++ JobScheduler/Data/Node.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace JobScheduler.Data
+{
+    public class Node
+    {
+        public IPAddress IP { get; set; }
+        public int Group { get; set; }
+    }
+}
